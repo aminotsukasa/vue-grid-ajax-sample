12c70170c330ecbd5a34ede5225bf4cefa877a30
冨田修平 <tomitashuuhei@tomitashuuhei-no-MacBook.local>
2017-06-17 16:15:46 +0900

    init

---
+++ list.php
@@ -0,0 +1,19 @@
+<?php
+
+echo json_encode(
+
+    [
+        [
+            "branch" => "master",
+            "bin_start" => "0",
+            "bin_end" => "199",
+        ],
+        [
+            "branch" => "feature-hoge",
+            "bin_start" => "200",
+            "bin_end" => "250",
+        ],
+    ]
+
+);
+
